b78d68fcfba7282fe08bb8b18a88c97cceeaa774
choisunghwan <dute7570@naver.com>
2023-01-25 14:24:14 +0900

    todoinput css

--- src/components/todo/TodoInput.js
+++ src/components/todo/TodoInput.js
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { MdAdd } from 'react-icons/md';
+import cn from 'classnames';
 import './css/TodoInput.css';
-import{ MdAdd } from 'react-icons/md';
+
 
 const TodoInput = () => {
+  
+  // todo-input 박스를 렌더링할지 여부
+  const [open, setOpen] = useState(false);
+
+  // todo-input 박스를 열고 닫는 클릭이벤트 핸들러
+  const inputToggle = () => setOpen(!open);
+
   return (
     <>
-        <div className='todo-input'>
-            <form className='insert-form'>
-                <input 
-                type="text"
-                placeholder='할 일을 입력후, 엔터를 누르세요!'
-                autoFocus
-                />
-            </form>
-        </div>
-        <button className='begin-btn'>
+        { open &&
+            <div className="todo-input">
+                <form className="insert-form">
+                    <input 
+                        type="text"
+                        placeholder="할 일을 입력 후, 엔터를 누르세요!"
+                        autoFocus
+                    />            
+                </form>
+            </div>
+        }
+
+        <button className={cn('begin-btn',{open})} onClick={inputToggle}>
             <MdAdd />
         </button>
     </>
