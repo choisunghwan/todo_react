961507b733e260257c5766eaed93deb9e812f9e6
choisunghwan <dute7570@naver.com>
2023-01-25 12:11:26 +0900

    # todoinput css

--- src/components/todo/TodoInput.js
+++ src/components/todo/TodoInput.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import './css/TodoInput.css';
+import{ MdAdd } from 'react-icons/md';
 
 const TodoInput = () => {
   return (
-    <div>TodoInput</div>
-  )
-}
+    <>
+        <div className='todo-input'>
+            <form className='insert-form'>
+                <input 
+                type="text"
+                placeholder='할 일을 입력후, 엔터를 누르세요!'
+                autoFocus
+                />
+            </form>
+        </div>
+        <button className='begin-btn'>
+            <MdAdd />
+        </button>
+    </>
+  );
+};
 
-export default TodoInput
+export default TodoInput;
