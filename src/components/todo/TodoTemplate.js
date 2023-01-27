8f3d9843adab84448cbd1f5d93bdd1e5c6496209
choisunghwan <dute7570@naver.com>
2023-01-27 15:47:15 +0900

    # todo 인증 토큰 헤더 추

--- src/components/todo/TodoTemplate.js
+++ src/components/todo/TodoTemplate.js
@@ -6,24 +6,33 @@ import './css/TodoTemplate.css';
 import TodoHeader from './TodoHeader';
 import TodoInput from './TodoInput';
 import TodoMain from './TodoMain';
-import { BASE_URL,TODO } from '../../config/host-config';
+
+import { BASE_URL, TODO } from '../../config/host-config';
+
+import { getToken } from '../util/login-util';
 
 const TodoTemplate = () => {
 
-  const API_BASE_URL = BASE_URL+TODO;
+  const API_BASE_URL = BASE_URL + TODO;
+  const ACCESS_TOKEN = getToken();
 
   // 할일 api데이터
   const [todos, setTodos] = useState([]);
 
-  //로딩중 상태 
-  const [loading,setLoading] = useState(true);
+  // 로딩중 상태
+  const [loading, setLoading] = useState(true);
+
+  const headerInfo = {
+    'content-type': 'application/json' 
+    , 'Authorization': 'Bearer ' + ACCESS_TOKEN 
+  };
 
   // 할 일 등록 서버 요청
   const addTodo = (todo) => {
 
     fetch(API_BASE_URL, {
         method: 'POST',
-        headers: { 'content-type': 'application/json' },
+        headers: headerInfo,
         body: JSON.stringify(todo)
     })
     .then(res => res.json())
@@ -36,7 +45,8 @@ const TodoTemplate = () => {
   const deleteTodo = (id) => {
 
     fetch(`${API_BASE_URL}/${id}`, {
-        method: 'DELETE'
+        method: 'DELETE',
+        headers: headerInfo
     })
     .then(res => res.json())
     .then(result => {
@@ -49,7 +59,7 @@ const TodoTemplate = () => {
 
     fetch(`${API_BASE_URL}/${todo.id}`, {
         method: 'PUT',
-        headers: { 'content-type': 'application/json' },
+        headers: headerInfo,
         body: JSON.stringify(todo)
     })
     .then(res => res.json())
@@ -62,16 +72,18 @@ const TodoTemplate = () => {
   // 렌더링되자마자 할 일 => todos api GET 목록 호출
   useEffect(() => {
 
-    fetch(API_BASE_URL)
-        .then(res =>{
-          if(res.status === 403){
+    fetch(API_BASE_URL, {
+        method: 'GET',
+        headers: headerInfo
+    })
+        .then(res => {
+          if (res.status === 403) {
             alert('로그인이 필요한 서비스입니다!');
-            // setLoading(false);
-            //리다이렉트
+            // 리다이렉트
             window.location.href = '/login';
             return;
-          }else if(res.status ===500){
-            alert('서버가 불안정합니다.');
+          } else if (res.status === 500) {
+            alert('서버가 불안정합니다 ㅈㅅ');
             return;
           }
           return res.json();
@@ -80,33 +92,34 @@ const TodoTemplate = () => {
             // console.log(result.todos);
             setTodos(result.todos);
 
-            // 로딩 완료처리
+            // 로딩완료처리
             setLoading(false);
         });
 
   }, []);
 
-  //로딩중일 때 보여줄 태그
+  // 로딩중일 때 보여줄 태그
   const loadingPage = (
-    <div className='loading'>
-        <Spinner color="danger">
-          Loading...
-        </Spinner>
+    <div className="loading">
+      <Spinner color="danger">
+        Loading...
+      </Spinner>
     </div>
-  )
-
-  //로딩 완료시 보여줄 태그
-  const viewPage = (
-      <div className="todo-template">
-      <TodoHeader todoList={todos} />
-      <TodoMain 
-          todoList={todos} 
-          remove={deleteTodo} 
-          update={updateTodo} 
-      />
-      <TodoInput add={addTodo} />
-  </div>
   );
+
+  // 로딩완료시 보여줄 태그
+  const viewPage = (
+    <div className="todo-template">
+        <TodoHeader todoList={todos} />
+        <TodoMain 
+            todoList={todos} 
+            remove={deleteTodo} 
+            update={updateTodo} 
+        />
+        <TodoInput add={addTodo} /> 
+    </div>
+  );
+  
 
   return (
     <>
